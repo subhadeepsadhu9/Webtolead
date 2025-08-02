c14fb443129a905d2465b6d879e07216fab31559
Subhadeep Sadhu <subhadeps@gmail.com>
2025-08-02 13:39:40 +0530

    recaptcha valdi

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function beforesubmit(){
-    console.log('in beforesubmit');
+function beforesubmit(event){
+    if(captchacheck){
+        console.log('in beforesubmit');
     let outputdate = document.querySelector('.outputDate');
     let inputdate = document.querySelector('.inputDate');
     console.log(inputdate);
     let formatteddate = new Date(inputdate.value).toLocaleDateString('en-IN');
     outputdate.value = formatteddate;
+    }
+    else{
+        alert('check the captcha');
+        event.preventDefault();
+    }
+    
+}
+
+let captchacheck = false;
+
+function captchasuccess(){
+    captchacheck = true;
 }
